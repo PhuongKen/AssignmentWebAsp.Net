7b30bb87cbde39685fac7bfa7a099ee50369835d
phuongken <you@example.com>
2018-11-23 23:03:04 +0700

    First Post Assignment Asp .NET Web Aplication

---
+++ AssignmentWebASP.NET/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using AssignmentWebASP.NET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignmentWebASP.NET.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult Register()
+        {
+            Customer customerModel = new Customer();
+            return View(customerModel);
+        }
+
+        [HttpPost]
+        public ActionResult Register(Customer customerModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                if (dbModel.Customers.Any(x => x.email == customerModel.email))
+                {
+                    ViewBag.DuplicateMessage = "Tài khoản này đã tồn tại";
+                    return View("Register");
+                }
+                customerModel.createAt = DateTime.Now;
+                dbModel.Customers.Add(customerModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Đăng ký thành công";
+            return View("Register", new Customer());
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Customer customerModel)
+        {
+            using (DbModels db = new DbModels())
+            {
+                var email = db.Customers.Where(x => x.email == customerModel.email && x.password == customerModel.password).FirstOrDefault();
+                if (email == null)
+                {
+                    customerModel.LoginErrorMessage = "Sai tài khoản hoặc mật khẩu.";
+                    return View("Login", customerModel);
+                }
+                else
+                {
+                    Session["id"] = email.id;
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+        }
+
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ AssignmentWebASP.NET/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using AssignmentWebASP.NET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignmentWebASP.NET.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin adminModel)
+        {
+            using (DbModels db = new DbModels())
+            {
+                var email = db.Admins.Where(x => x.email == adminModel.email && x.password == adminModel.password).FirstOrDefault();
+                if (email == null)
+                {
+                    adminModel.LoginErrorMessage = "Sai tài khoản hoặc mật khẩu.";
+                    return View("Login", adminModel);
+                }
+                else
+                {
+                    Session["AdminID"] = email.id;
+                    return RedirectToAction("Index", "Admin");
+                }
+
+            }
+        }
+
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Admin");
+        }
+
+    }
+}
